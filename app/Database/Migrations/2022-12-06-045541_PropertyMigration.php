394565d920d868cfd6fc355ed24b2fabc3ef2eeb
r1z4ld1 <linuxbacktrack737@gmail.com>
2022-12-06 13:42:42 +0800

    menambah file migrasi tabel [property]

---
+++ app/Database/Migrations/2022-12-06-045541_PropertyMigration.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class PropertyMigration extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_property' => [
+                'type' => 'INT',
+                'constraint' => '5',
+                'unsiged' => true,
+                'auto_increment' => true,
+            ],
+            'type_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '75',
+            ],
+            'address' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'post_number' => [
+                'type' => 'VARCHAR',
+                'constraint' => '10',
+            ],
+            'lt' => [
+                'type' => 'INT',
+                'constraint' => '5',
+            ],
+            'lb' => [
+                'type' => 'INT',
+                'constraint' => '5',
+            ],
+            'area' => [
+                'type' => 'INT',
+                'constraint' => '5',
+            ],
+            'bads' => [
+                'type' => 'INT',
+                'constraint' => '2',
+            ],
+            'baths' => [
+                'type' => 'INT',
+                'constraint' => '2',
+            ],
+            'garages' => [
+                'type' => 'INT',
+                'constraint' => '2',
+            ],
+            'description' => [
+                'type' => 'VARCHAR',
+                'constraint' => '650',
+            ],
+            'aminities' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'video' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'img_card' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'img_spec1' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'img_spec2' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'id_property_spec' => [
+                'type' => 'INT',
+                'constraint' => '5',
+            ],
+            'id_property_gallery' => [
+                'type' => 'INT',
+                'constraint' => '5',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+
+        ]);
+        $this->forge->addKey('id_property', true);
+        $this->forge->createTable('property');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('property');
+    }
+}
