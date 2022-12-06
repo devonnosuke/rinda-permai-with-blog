89d6ad3ce6cffbb22ddffb1a2b2327d92986faef
r1z4ld1 <linuxbacktrack737@gmail.com>
2022-12-06 15:04:28 +0800

    menambah file migrasi tabel user/login

---
+++ app/Database/Migrations/2022-12-06-070037_UserMigration.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UserMigration extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_user' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'username' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'password' => [
+                'type' => 'TEXT',
+            ],
+        ]);
+        $this->forge->addKey('id_user', true);
+        $this->forge->createTable('user');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('user');
+    }
+}
