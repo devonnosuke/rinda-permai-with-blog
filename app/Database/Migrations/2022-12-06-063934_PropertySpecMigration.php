b699c6632bde95ccbe3c7f21f924e9a27510e5e9
Mauliani <mauliani@gmail.com>
2022-12-06 14:52:02 +0800

    menambah file seeder tabel property_gallery dn property_spec

---
+++ app/Database/Migrations/2022-12-06-063934_PropertySpecMigration.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class PropertySpecMigration extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_property_spec' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'property_spec' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'specification' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'NULL' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'NULL' => true,
+            ],
+        ]);
+        $this->forge->addKey('id_property_spec', true);
+        $this->forge->createTable('property_spec');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('property_spec');
+    }
+}
