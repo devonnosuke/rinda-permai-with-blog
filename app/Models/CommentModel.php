f391329e03e3d772ce9ac6ad108218b863a447bb
chyntiawidya <chyntiawidyanigrum@gmail.com>
2022-12-08 17:12:36 +0800

    ganti nama class comment

--- app/Models/CommentModel.php
+++ app/Models/CommentModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class StudentModel extends Model
+class CommentModel extends Model
 {
     protected $table            = 'comment';
     protected $primaryKey       = 'id_comment';
