dcae7d8aec40dfaf79763aa41885d1ff23a2eb96
josestohama <josestohama@gmail.com>
2022-12-09 20:07:09 -0800

    mengedit views public facility

--- app/Views/admin/PublicFacilityView.php
+++ app/Views/admin/PublicFacilityView.php
@@ -5,6 +5,7 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-2 text-gray-800">Data Public Facility</h1>
+    <?php d($PublicFacility); ?>
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
@@ -15,6 +16,7 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
+                            <th>No</th>
                             <th>Name</th>
                             <th>Descrition</th>
                             <th>Icon</th>
@@ -22,66 +24,28 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Tiger Nixon</td>
-                            <td class="text-center">
-                                <img src="<?= base_url(); ?>/img/undraw_profile.svg" alt="" class="img-thumbnail img-fluid" style="width: 150px" />
-                            </td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
-                            <td>2011/04/25</td>
-                            <td class="text-center">
-                                <a href="<?= base_url(); ?>nama_tabel/edit/" class="btn btn-warning btn-circle me-2">
-                                    <i class="fas fa-edit"></i>
-                                </a>
-                                <form action="<?= base_url(); ?>nama_tabel/delete/" method="post" style="display: inline">
-                                    <button class="btn btn-danger ms-2 btn-circle">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                    <input type="hidden" name="_method" value="DELETE" />
-                                </form>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Michael Bruce</td>
-                            <td class="text-center">
-                                <img src="<?= base_url(); ?>/img/undraw_profile.svg" alt="" class="img-thumbnail img-fluid" style="width: 150px" />
-                            </td>
-                            <td>Singapore</td>
-                            <td>29</td>
-                            <td>2011/06/27</td>
-                            <td class="text-center">
-                                <a href="<?= base_url(); ?>nama_tabel/edit/" class="btn btn-warning btn-circle me-2">
-                                    <i class="fas fa-edit"></i>
-                                </a>
-                                <form action="<?= base_url(); ?>nama_tabel/delete/" method="post" style="display: inline">
-                                    <button class="btn btn-danger ms-2 btn-circle">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                    <input type="hidden" name="_method" value="DELETE" />
-                                </form>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Donna Snider</td>
-                            <td class="text-center">
-                                <img src="<?= base_url(); ?>/img/undraw_profile.svg" alt="" class="img-thumbnail img-fluid" style="width: 150px" />
-                            </td>
-                            <td>New York</td>
-                            <td>27</td>
-                            <td>2011/01/25</td>
-                            <td class="text-center">
-                                <a href="<?= base_url(); ?>nama_tabel/edit/" class="btn btn-warning btn-circle me-2">
-                                    <i class="fas fa-edit"></i>
-                                </a>
-                                <form action="<?= base_url(); ?>nama_tabel/delete/" method="post" style="display: inline">
-                                    <button class="btn btn-danger ms-2 btn-circle">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                    <input type="hidden" name="_method" value="DELETE" />
-                                </form>
-                            </td>
-                        </tr>
+                        <?php $no = 1; ?>
+                        <?php foreach ($PublicFacility as $PublicFacility) : ?>
+                            <tr>
+                                <td><?= $no++; ?></td>
+                                <td><?= $PublicFacility['name']; ?></td>
+                                <td><?= $PublicFacility['description']; ?></td>
+                                <td class="text-center">
+                                    <img src="<?= base_url(); ?>/img/undraw_profile.svg" alt="" class="img-thumbnail img-fluid" style="width: 150px" />
+                                </td>
+                                <td class="text-center">
+                                    <a href="<?= base_url(); ?>nama_tabel/edit/" class="btn btn-warning btn-circle me-2">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <form action="<?= base_url(); ?>nama_tabel/delete/" method="post" style="display: inline">
+                                        <button class="btn btn-danger ms-2 btn-circle">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                        <input type="hidden" name="_method" value="DELETE" />
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
             </div>
