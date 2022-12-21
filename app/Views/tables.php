b0b9817d10288801761563ba09508d687baee528
josestohama <josestohama@gmail.com>
2022-12-21 03:20:29 -0800

    menambah menu public facility di navbar

--- app/Views/tables.php
+++ app/Views/tables.php
@@ -68,14 +68,14 @@
 
 
             <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
+            <li class="nav-item active">
+                <a class="nav-link" href="<?= base_url(); ?>/admin/public-facility">
+                    <i class="bi bi-people-fill"></i>
+                    <span>Public Facility</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href="tables.html">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Tables</span></a>
