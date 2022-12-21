67c4477f38bd73bf4aafa22a738ed2bc3ce79ff2
aestrid <astridsalim97@gmail.com>
2022-12-21 16:32:14 +0800

    membuat create tabel about

--- app/Controllers/AboutController.php
+++ app/Controllers/AboutController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use Config\Services;
 
 class AboutController extends BaseController
 {
@@ -16,5 +17,35 @@ class AboutController extends BaseController
         $data['About'] = $model->findAll();
 
         return view('admin/AboutView', $data);
+    }
+    public function add()
+    {
+        $data['title'] = 'Tambah About📑';
+        $data['validation'] = Services::validation();
+        return view('admin/about-add', $data);
+    }
+    public function save($id = null)
+    {
+        $model = new \App\Models\AboutModel();
+        $data = $this->request->getVar();
+        if ($id == true) {
+            $data['id_about'] = $id;
+        }
+
+        $cek = $model->save($data);
+
+        if ($cek == true) {
+            return redirect()->to('/admin/about');
+        }
+        return "Gagal Disimpan!";
+    }
+    public function delete($id = null)
+    {
+        $model = new \App\Models\AboutModel();
+        $cek = $model->delete($id);
+        if ($cek == true) {
+            return redirect()->to('/admin/about');
+        }
+        return "Gagal Dihapus!";
     }
 }
