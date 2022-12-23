20c6b65d088fe5f8f897381bbe2b46c8564d746a
r1z4ld1 <linuxbacktrack737@gmail.com>
2022-12-23 15:46:41 +0800

    mengedit userseeder

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -8,17 +8,15 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        for ($i = 0; $i  < 6; $i++) {
+        // password asli [RiaN2020]
+        $data = [
+            'username'      => "rian-ryaldi",
+            'password'      => "21635906b2fa78f4ec7c2d646a443b1f",
+            'created_at'    => date('Y-m-d H:i:s'),
+            'updated_at'    => date('Y-m-d H:i:s'),
+        ];
 
-            $data = [
-                'username'      => "$i-Ryanch",
-                'password'      => "ryanch",
-                'created_at'    => date('Y-m-d H:i:s'),
-                'updated_at'    => date('Y-m-d H:i:s'),
-            ];
-
-            // using query builder
-            $this->db->table('user')->insert($data);
-        }
+        // using query builder
+        $this->db->table('user')->insert($data);
     }
 }
